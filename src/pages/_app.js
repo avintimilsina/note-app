fa8c5ef63c444afdee9ad8cc56dbfefb14ebb65c
Avin-T <avin.timilsina.16@gmail.com>
2023-03-27 23:48:20 +0545

    Created note app layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import { AppProps } from "next/app";
+import "../styles/globals.css";
+import { Toaster } from "react-hot-toast";
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Component {...pageProps} />
+			<Toaster/>
+		</>
+	);
 }
