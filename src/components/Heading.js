8f572b0c297d085c1833a86966d08d649f27e33d
avintimilsina <avin.timilsina.16@gmail.com>
2023-04-17 17:55:34 +0545

    style(pages): :lipstick: added webpage title and logo

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -2,16 +2,16 @@ import Image from "next/image";
 import React from "react";
 
 const Heading = ({ headingText }) => {
-	return (
-		<div className="flex flex-row justify-center">
-			<div className=" card mx-10 my-7 w-96 bg-base-100 text-center text-3xl font-bold  shadow-xl">
-				<a href="#" class=" flex items-center  justify-center">
-					<p className="better-font size px-4 text-5xl">{headingText}</p>
-					<Image height={100} width={100} src="logo.svg" alt="logo" />
-				</a>
-			</div>
-		</div>
-	);
+    return (
+        <div className="flex flex-row justify-center">
+            <div className=" card mx-10 my-7 w-96 bg-base-100 text-center text-3xl font-bold  shadow-xl">
+                <a href="#" class=" flex items-center  justify-center">
+                    <p className="better-font size px-4 text-5xl">{headingText}</p>
+                    <Image height={100} width={100} src="logo.svg" alt="logo" />
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default Heading;
--- src/components/Note.js
+++ src/components/Note.js
@@ -5,92 +5,90 @@ import { toast } from "react-hot-toast";
 import { db } from "../../firebase";
 
 const Note = ({ heading, message, createdAt, id }) => {
-	const deleteNote = async (id) => {
-		await deleteDoc(doc(db, "notes", id));
-	};
+    const deleteNote = async (id) => {
+        await deleteDoc(doc(db, "notes", id));
+    };
 
-	return (
-		<div
-			className="card mx-10 my-5 w-96 bg-base-100 shadow-xl "
-			key={createdAt}
-		>
-			<div className="card-body ">
-				<h1 className="card-title overflow-hidden">{heading}</h1>
-				<p className="overflow-hidden">{message}</p>
-				<p>Created {dayjs(createdAt).fromNow(true)} ago</p>
+    return (
+        <div
+            className="card mx-10 my-5 w-96 bg-base-100 shadow-xl "
+            key={createdAt}
+        >
+            <div className="card-body ">
+                <h1 className="card-title overflow-hidden">{heading}</h1>
+                <p className="overflow-hidden">{message}</p>
+                <p>Created {dayjs(createdAt).fromNow(true)} ago</p>
 
-				<div className="card-actions justify-between">
-					<Formik
-						initialValues={{
-							heading: heading,
-							message: message,
-						}}
-						onSubmit={async (editValue) => {
-							console.log("hello");
-							const docRef = doc(db, "notes", id);
-							await updateDoc(docRef, {
-								heading: editValue.heading,
-								message: editValue.message,
-							});
-							toast.success("Note edited successfully!");
-							// console.log("Form Submitted");
-							// router.reload();
-						}}
-					>
-						<Form>
-							<label
-								htmlFor={id}
-								className="hover: ... btn-primary btn-sm btn transition delay-75 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
-							>
-								View
-							</label>
+                <div className="card-actions justify-between">
+                    <Formik
+                        initialValues={{
+                            heading: heading,
+                            message: message,
+                        }}
+                        onSubmit={async (editValue) => {
+                            console.log("hello");
+                            const docRef = doc(db, "notes", id);
+                            await updateDoc(docRef, {
+                                heading: editValue.heading,
+                                message: editValue.message,
+                            });
+                            toast.success("Note edited successfully!");
+                        }}
+                    >
+                        <Form>
+                            <label
+                                htmlFor={id}
+                                className="hover: ... btn-primary btn-sm btn transition delay-75 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
+                            >
+                                View
+                            </label>
 
-							<input type="checkbox" id={id} className="modal-toggle" />
-							<label htmlFor={id} className="modal cursor-pointer">
-								<label
-									className="modal-box relative w-8/12 max-w-2xl"
-									htmlFor=""
-								>
-									<h3 className="text-lg font-semibold">
-										<Field
-											id="heading"
-											name="heading"
-											placeholder="Heading"
-											className="w-full bg-transparent text-2xl !outline-none "
-										/>
-									</h3>
-									<p className="py-4">
-										<Field
-											as="textarea"
-											id="message"
-											name="message"
-											placeholder="Message"
-											className="h-24 w-full bg-transparent !outline-none"
-										/>
-									</p>
-									<button
-										className="hover: ... btn-primary btn-sm btn transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
-										type="submit"
-									>
-										Edit
-									</button>
-								</label>
-							</label>
-						</Form>
-					</Formik>
+                            <input type="checkbox" id={id} className="modal-toggle" />
+                            <label htmlFor={id} className="modal cursor-pointer">
+                                <label
+                                    className="modal-box relative w-8/12 max-w-2xl"
+                                    htmlFor=""
+                                >
+                                    <h3 className="text-lg font-semibold">
+                                        <Field
+                                            id="heading"
+                                            name="heading"
+                                            placeholder="Heading"
+                                            className="w-full bg-transparent text-2xl !outline-none "
+                                        />
+                                    </h3>
+                                    <p className="py-4">
+                                        <Field
+                                            as="textarea"
+                                            id="message"
+                                            name="message"
+                                            placeholder="Message"
+                                            className="h-24 w-full bg-transparent !outline-none"
+                                        />
+                                    </p>
+                                    <button
+                                        className="hover: ... btn-primary btn-sm btn transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
+                                        type="submit"
+                                    >
+                                        Edit
+                                    </button>
+                                </label>
+                            </label>
+                        </Form>
+                    </Formik>
 
-					<button
-						className="hover: ... btn-error btn-sm btn transition delay-75 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
-						onClick={() => {
-							deleteNote(id);
-						}}
-					>
-						Delete
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+                    <button
+                        className="hover: ... btn-error btn-sm btn transition delay-75 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110"
+                        onClick={() => {
+                            deleteNote(id);
+                        }}
+                    >
+                        Delete
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Note;
